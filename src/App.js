420036d00bcca71b75abf0569b20990138fb0f0e
afahgan <afahgan@students.iugaza.edu.ps>
2022-04-19 21:30:40 +0300

    Create Anime Page #5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import AnimeCard from "./Component/Anime/AnimeCard";
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import AnimePage from "./Component/Anime Page/AnimePage";
+const id = 0; 
 function App() {
   return (
     <BrowserRouter>
       <div>
           <Routes>
             <Route path="/" element={<AnimeCard title="one-piece" />} />
-            <Route path="/anime/:animeID" element={<AnimeCard title="naruto" />} />
+            <Route path="/anime/:animeID" element={<AnimePage id={id} />} />
           </Routes>
       </div>
     </BrowserRouter>
