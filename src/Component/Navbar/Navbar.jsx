55048704a89710ca58ef9976db9b99419fe43e08
afahgan <afahgan@students.iugaza.edu.ps>
2022-04-20 15:37:33 +0300

    Create Home Page #9

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -8,12 +8,17 @@ export const Navbar = ({searchTerm,setSearchTerm}) => {
 
 
     return(
-        <nav className="navbar">
-            <img className="logo" src="https://media.discordapp.net/attachments/940611625122156605/965937273432592394/MyAnimeList.png" alt="NOT FOUND IMAGEZ" />
+      <div>
+          <nav className="navbar">
+          <a href="/" className="navbar-brand">
+          <img  className="logo" src="https://media.discordapp.net/attachments/940611625122156605/965937273432592394/MyAnimeList.png" alt="NOT FOUND IMAGEZ" />
+          </ a>
             <input value={searchTerm} onChange={handleChange} name="searchWords"  className="search" type="search" placeholder="Search.."/>
             
-            <button className="login">login</button>
+            <button className="login">Login</button>
         </nav>
+        <div className='fix'></div>
+      </div>
         
       
     )
